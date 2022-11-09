ecd3bea39b3f09611068daa42cc198052aa2a301
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2022-11-08 22:45:24 -0300

    add template string

--- Fundamentos/fundamentos/strings.js
+++ Fundamentos/fundamentos/strings.js
@@ -5,8 +5,22 @@ console.log(jogador.charAt(1))
 console.log(jogador.charAt(2))
 console.log(jogador.charAt(3))
 console.log(jogador.charCodeAt(2))//pega o valor na tabela ascii
-console.log(jogador.indexOf('t'))//indice da letra passada
+console.log(jogador.indexOf('o'))//indice da letra passada
 console.log(jogador.substring(1))//vai a partir do indice 1 até o final
 console.log('Jogador: '.concat(jogador).concat(' é o melhor jogador do mundo!'))
 console.log(jogador.replace('e', 3))
 console.log('João, Maria, José'.split(','))//converter de string pra array
+
+const nome = 'João'
+let concatenacao = 'Olá ' + nome + '!'
+let templateString = `
+    Olá
+    ${nome}!
+`//tamplate string é através da crase!
+
+console.log(concatenacao)
+console.log(templateString)
+
+//podemos chamar funções com template string!
+const maiusculo = texto => texto.toUpperCase()
+console.log(`Ei... ${maiusculo('cuidado!')}`)
