5cea47ec70cc6e4fef73ffa0d88fb6e4e389f8db
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2022-11-08 21:41:05 -0300

    .

---
+++ Fundamentos/inicio/desafioAreaCirc.js
@@ -0,0 +1,7 @@
+let raio = 23
+const pi = 3.14
+
+let areaCircunferencia = pi * raio * raio
+
+console.log("A área da circunferência é " + areaCircunferencia + " metros quadrados")
+console.log("E o tipo é um(a) " + typeof areaCircunferencia)
---
+++ Fundamentos/inicio/desafioTroca.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 23
+let temporario = a
+
+a = b
+b = temporario
+
+console.log(a)
+console.log(b)
