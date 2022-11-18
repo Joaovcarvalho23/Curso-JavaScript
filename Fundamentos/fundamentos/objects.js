2190c1fd3026727fb87a18e0406ed3839974ea32
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2022-11-18 00:17:11 -0300

    add object basico

---
+++ Fundamentos/fundamentos/objects.js
@@ -0,0 +1,12 @@
+const produto1 = {}
+produto1.nome = 'Notebook Acer Nitro 5'
+produto1.preco = 4999.90
+produto1.nota = 10
+console.log(produto1)
+
+const produto2 = {
+    nome: 'Iphone 13 Pro MAX',
+    preco: 6998.90,
+    nota: 9.5
+}
+console.log(produto2)
