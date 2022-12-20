10b080efccab0acaf640e6d5db081c8e2a5485d6
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2022-12-20 20:54:55 -0300

    aritmetica basica em js

---
+++ Fundamentos/fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [10, 12, 23, 30]
+
+const soma = a + b + c + d
+const subtracao = d - c
+const divisao = d / 2
+const multiplicacao = b * a
+const modulo = a % 2
+
+console.log(soma, subtracao, divisao, multiplicacao, modulo)
