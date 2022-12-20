4249d8a25480b85c3cdc6e82fd5d3e1cee4af278
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2022-12-20 18:36:02 -0300

    atribuicao em js

---
+++ Fundamentos/fundamentos/atribuicao.js
@@ -0,0 +1,16 @@
+const a = 7
+let b = 16
+b += a// b = b+a
+console.log(b)
+
+b -= a// b = b - a
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 //b = b % 2
+console.log(b)
