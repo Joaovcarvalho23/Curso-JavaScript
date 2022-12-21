9e1d05e94da60793887acab50936c74b6984bb29
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2022-12-20 21:57:49 -0300

    dicas de operações unárias em js

---
+++ Fundamentos/fundamentos/unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--) //evitar fazer isso para o código ficar mais claro!
+console.log(num1 === num2)
