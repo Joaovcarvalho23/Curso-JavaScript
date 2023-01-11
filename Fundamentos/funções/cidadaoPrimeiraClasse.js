3d5c8a700e8802a518902f67eb4a3e9914ba6593
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-01-11 17:43:16 -0300

    tipos de criação de função em js

---
+++ Fundamentos/funções/cidadaoPrimeiraClasse.js
@@ -0,0 +1,30 @@
+//Criando função...
+
+//De forma literal
+function fun1(){}
+
+//Armazenar em uma variável
+const fun2 = function() { }
+
+//Armazenar em um array
+const meuArray = [function (a, b) {return a + b}, fun1, fun2]
+console.log(meuArray[0](10, 13))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () {return 'Opa! Tudo bem?'}
+console.log(obj.falar())
+
+//Passar função como parâmetro
+function rodar(fun){
+    fun()
+}
+
+rodar(function () {console.log('Executando...')})
+
+function soma(a, b){
+    return function (c){
+        console.log(a + b + c)
+    }
+}
+soma(2, 3)(15)
