aad32a2a462e15c8c89830cc73ecf48e09e3f74d
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-01-25 14:08:11 -0300

    questao 25 feita

---
+++ Fundamentos/Lista de exercicios 1/Questões/q25.js
@@ -0,0 +1,16 @@
+/*25) Escrever um programa para exibir os números de 1 até 50 na tela.*/
+
+function imprimeNumeros (){
+    
+for (let i = 1; i <= 50; i++){
+    console.log(i)
+}
+    /*pode ser feito assim
+    let i = 1
+    while(i <= 50){
+        console.log(i)
+        i++
+    }
+    */
+}
+imprimeNumeros()
